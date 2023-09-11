623dc704f8c471c74e0ded9e7d343abfc11a99a9
Tyler Garner <tyler@garnercode.io>
2023-09-11 16:45:27 -0500

    Testing CORS fix for front-end API fetching

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 
 mongoose.connect(
@@ -13,6 +14,12 @@ mongoose.connect(
 
 const api_users = require("./api/users");
 
+app.use(cors({
+    // origin: [
+    //     'https://lorelock.vercel.app',
+    // ]
+    origin: '*'
+}));
 app.use(express.json());
 app.use('/users', api_users);
 app.listen(process.env.PORT || 3000);
